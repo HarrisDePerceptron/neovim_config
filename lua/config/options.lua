6a0558233ba4da17933bf2c9f00406dd115f7c9e
Muhammad Harris <harris.perceptron@gmail.com>
2024-03-16 01:01:31 +0500

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+-- opts = function(_, opts)
+--  local cmp = require("cmp")
+--  opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+--    { name = "crates" },
+--  }))
+--end
+
+vim.opt.relativenumber = false
